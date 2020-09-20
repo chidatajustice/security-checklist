ccb12f9676a5dffcca7a1e2a21873632bb887157
Shaka P <shakalee@Shakas-MacBook-Pro-2.local>
2020-09-20 15:13:50 -0500

    remove checklist url

--- components/ShareButtons/index.js
+++ components/ShareButtons/index.js
@@ -7,7 +7,7 @@ export default function ShareButtons() {
   return (
     <Container>
       <FacebookButton
-        href="https://www.facebook.com/sharer/sharer.php?u=https://securitycheckli.st"
+        href="https://www.facebook.com/sharer/sharer.php?u=https://fierce-retreat-27011.herokuapp.com/"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -15,14 +15,14 @@ export default function ShareButtons() {
       </FacebookButton>
 
       <TwitterButton
-        href="http://twitter.com/share?text=Check out Security Checklist, a checklist to help people stay safe online&url=https://securitycheckli.st"
+        href="http://twitter.com/share?text=Check out Security Checklist, a checklist to help people stay safe online&url=https://fierce-retreat-27011.herokuapp.com/"
         target="_blank"
         rel="noopener noreferrer"
       >
         Tweet
       </TwitterButton>
 
-      <CopyLinkButton text="https://securitycheckli.st">
+      <CopyLinkButton text="https://fierce-retreat-27011.herokuapp.com/">
         Share Link
       </CopyLinkButton>
     </Container>
--- config/next-seo.js
+++ config/next-seo.js
@@ -4,14 +4,14 @@ export default {
   openGraph: {
     type: 'website',
     locale: 'en_US',
-    url: 'https://securitycheckli.st/',
+    url: 'https://fierce-retreat-27011.herokuapp.com/',
     title: 'Security Checklist',
     description: 'A checklist for staying safe on the internet',
     defaultImageWidth: 512,
     defaultImageHeight: 512,
     images: [
       {
-        url: 'https://securitycheckli.st/static/og-image.png',
+        url: 'https://fierce-retreat-27011.herokuapp.com/static/og-image.png',
         width: 512,
         height: 512,
         alt: 'Security Checklist',
