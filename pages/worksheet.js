5fda806e469f5ac31010fff4bfa7b4aba38b6333
haworku <haworku@users.noreply.github.com>
2020-10-24 13:51:05 -0500

    make fields editable

--- pages/worksheet.js
+++ pages/worksheet.js
@@ -15,40 +15,50 @@ export default function Worksheet() {
       <SectionHeading>
         <Heading>Digital Security Training Worksheet</Heading>
       </SectionHeading>
-      <br/>
-      <p>This worksheet is used to practice threat modeling and was originally designed for the D4BL digitial security training.</p>
-      <br/>
-      <Subheading>
-        Black LGBT youth living with conservative religious family
-        </Subheading>
-        <p>What information do I want to keep secret?</p>
-        <textarea style={{width:"100%", height: "10vh", margin: "10px 0px"}} value="My sexual and or gender identity, any relationships I may have"/>
-        <p>From whom?</p>
-        <textarea style={{width:"100%", height: "10vh", marginTop: "10px" }} value="My family members, members of my family’s church,  and their circle of friends"/>
-        <p>What tools do they have (to try and get the information)?</p>
-        <textarea style={{width:"100%", height: "10vh"}}/>
-        <p>How likely is it that I will need to protect it?</p>
-        <textarea style={{width:"100%", height: "10vh"}}/>
-        <p>What do I have to lose if the information gets out?</p>
-        <textarea style={{width:"100%", height: "10vh"}}/>
-        <p>What steps am I willing to take to protect information more:</p>
-        <textarea style={{width:"100%", height: "10vh"}}/>
+      <br />
+      <p>
+        This worksheet is used to practice threat modeling and was originally designed for the D4BL digitial security
+        training.
+      </p>
+      <br />
+      <Subheading style={{ margin: '25px 0px' }}>Black LGBT youth living with conservative religious family</Subheading>
+      <p>What information do I want to keep secret?</p>
+      <textarea
+        style={{ width: '100%', height: '10vh', margin: '10px 0px' }}
+        defaultValue="My sexual and or gender identity, any relationships I may have"
+      />
+      <p>From whom?</p>
+      <textarea
+        style={{ width: '100%', height: '10vh', marginTop: '10px' }}
+        defaultValue="My family members, members of my family’s church,  and their circle of friends"
+      />
+      <p>What tools do they have (to try and get the information)?</p>
+      <textarea style={{ width: '100%', height: '10vh' }} />
+      <p>How likely is it that I will need to protect it?</p>
+      <textarea style={{ width: '100%', height: '10vh' }} />
+      <p>What do I have to lose if the information gets out?</p>
+      <textarea style={{ width: '100%', height: '10vh' }} />
+      <p>What steps am I willing to take to protect information more:</p>
+      <textarea style={{ width: '100%', height: '10vh' }} />
 
-        <Subheading style={{ margin: "20px 0px", paddingTop: "20px" }}>
+      <Subheading style={{ margin: '20px 0px', paddingTop: '20px' }}>
         Black activist who is a leader in public protests
-        </Subheading>
-        <p>What information do I want to keep secret?</p>
-        <textarea style={{width:"100%", height: "10vh", margin: "10px 0px"}} value="My contact information and my parents contact information"/>
-        <p>From whom?</p>
-        <textarea style={{width:"100%", height: "10vh"}} value="Doxxers trying to harass me" />
-        <p>What tools do they have (to try and get the information)?</p>
-        <textarea style={{width:"100%", height: "10vh"}}/>
-        <p>How likely is it that I will need to protect it?</p>
-        <textarea style={{width:"100%", height: "10vh"}}/>
-        <p>What do I have to lose if the information gets out?</p>
-        <textarea style={{width:"100%", height: "10vh"}}/>
-        <p>What steps am I willing to take to protect information more:</p>
-        <textarea style={{width:"100%", height: "10vh"}}/>
+      </Subheading>
+      <p>What information do I want to keep secret?</p>
+      <textarea
+        style={{ width: '100%', height: '10vh', margin: '10px 0px' }}
+        defaultValue="My contact information and my parents contact information"
+      />
+      <p>From whom?</p>
+      <textarea style={{ width: '100%', height: '10vh' }} defaultValue="Strangers trying to harass me" />
+      <p>What tools do they have (to try and get the information)?</p>
+      <textarea style={{ width: '100%', height: '10vh' }} />
+      <p>How likely is that I will need to protect it?</p>
+      <textarea style={{ width: '100%', height: '10vh' }} />
+      <p>What do I have to lose if the information gets out?</p>
+      <textarea style={{ width: '100%', height: '10vh' }} />
+      <p>What steps am I willing to take to protect information more:</p>
+      <textarea style={{ width: '100%', height: '10vh' }} />
     </Page>
   );
 }
